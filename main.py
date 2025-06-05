3a25fbfbfef77ef316b732ccaa2a3aa951935fb7
Versianih <versianienzo@gmail.com>
2025-06-05 03:11:17 -0300

    rename call_api to solver.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from modules.data import collect_data
+
+seasons, exam_path = collect_data()
--- modules/select_file.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-import datetime
-
-def open_file_select() -> str:
-    tk.Tk().withdraw()
-    file_path = askopenfilename()
-    
-    return file_path
