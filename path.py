76f7b290b8e17809d1c013d10ff9e50da7ac18e6
Versianih <versianienzo@gmail.com>
2025-06-05 14:40:43 -0300

    add: tools.py para configurar via linha de comando

--- path.py
+++ path.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-
 class P:
-    output_path = Path('output/')
-    ai_path = Path('AI/')
-    modules_path = Path('modules/')
+    output_path = Path('./outputs')
+    ai_path = Path('./AI')
+    modules_path = Path('./modules')
---
+++ tools.py
@@ -0,0 +1,23 @@
+import sys, os, shutil
+from path import P
+
+line_command = sys.argv
+
+try:
+    if line_command[1] == '-h':
+        print('Help')
+
+    if line_command[1] == '-e' and line_command[2] == 'check':
+        print('Checando env')
+        pass
+
+    if line_command[1] == '-c' and line_command[2] == 'output':
+        if os.path.isdir(P.output_path):
+            shutil.rmtree(P.output_path)
+            os.makedirs('outputs')
+            print(f"Outputs limpos com sucesso.")
+        else:
+            os.makedirs('outputs')
+            print(f"Diretório de output não encontrado.")
+except:
+    print("try: tools.py -h, for help.")
